affdd420b84b2a705f186102d06e6c2b3f9ec118
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-12-09 02:08:50 -0300

    complementa controles

--- src/componentes/Controles.tsx
+++ src/componentes/Controles.tsx
@@ -1,16 +1,57 @@
 import "./Controles.css"
+
 interface ControlesProps {
     titulo: string;
+    onPedirCarta: () => void;
+    onParar: () => void;
+    onDobrarAposta: () => void;
+    onNovoJogo: () => void;
+    jogadorVez: boolean;
+    jogoAtivo: boolean;
+    podeDobrar: boolean;
 }
 
-const Controles = ({ titulo }: ControlesProps) => {
+const Controles = ({ 
+    titulo,
+    onPedirCarta,
+    onParar,
+    onDobrarAposta,
+    onNovoJogo,
+    jogadorVez,
+    jogoAtivo,
+    podeDobrar
+}: ControlesProps) => {
     return (
         <div className="controles">
             <h3>{titulo}</h3>
-            <div className="cartas">
-                <button className="botaocontrole">Pedir Carta</button>
-                <button className="botaocontrole">Parar</button>
-                <button className="botaocontrole">Dobrar Aposta</button>
+            <div className="botoes-controle">
+                <button 
+                    className="botaocontrole"
+                    onClick={onPedirCarta}
+                    disabled={!jogadorVez || !jogoAtivo}
+                >
+                    Pedir Carta
+                </button>
+                <button
+                    className="botaocontrole"
+                    onClick={onParar}
+                    disabled={!jogadorVez || !jogoAtivo}
+                >
+                    Parar
+                </button>
+                <button
+                    className="botaocontrole"
+                    onClick={onDobrarAposta}
+                    disabled={!jogadorVez || !jogoAtivo || !podeDobrar}
+                >
+                    Dobrar Aposta
+                </button>
+                <button 
+                    className="botaocontrole novo-jogo"
+                    onClick={onNovoJogo}
+                >
+                    Novo Jogo
+                </button>
             </div>
         </div>
     );
