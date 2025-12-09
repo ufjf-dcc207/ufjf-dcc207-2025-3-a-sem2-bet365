d72272ae87f2e9eda11014a09b075409e40ceb29
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-12-09 11:03:34 -0300

    tira saldo de dentro de mesablackjack

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import MesaBlackjack from './componentes/MesaBlackjack'
 import Saldo from './componentes/Saldo'
 
 function App() {
+  const [saldo, setSaldo] = useState(100)
+
+  const adicionarSaldo = () => {
+    setSaldo(saldo + 50)
+  }
+
+  const atualizarSaldo = (novoSaldo: number) => {
+    setSaldo(novoSaldo)
+  }
+
+
   return (
     <div className='App'>
       <h1>♠️ Bem-vindo ao jogo de 21 ♣️</h1>
       <MesaBlackjack />
+      <Saldo valor={saldo} onAdicionarSaldo={adicionarSaldo} aposta={0} />
     </div>
   )
 }
